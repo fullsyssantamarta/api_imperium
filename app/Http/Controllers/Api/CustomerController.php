932eedd88480ff94fa122533893c734fc6454beb
AndyTR03 <ndtrillo@gmail.com>
2025-01-20 17:36:01 -0500

    Servicios customers para la app

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -23,4 +23,34 @@ class CustomerController extends Controller
             ], 500);
         }
     }
+
+    public function store(Request $request)
+    {
+       try{
+
+        $validatedData = $request->validate([
+            'identification_number' => 'required|string|max:20|unique:customers',
+            'dv' => 'nullable|string|max:1',
+            'name' => 'required|string|max:500',
+            'phone' => 'nullable|string|max:20',
+            'address' => 'nullable|string|max:500',
+            'email' => 'nullable|email|max:255',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+        $customer = Customer::create($validatedData);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Cliente creado exitosamente',
+            'data' => $customer
+        ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
